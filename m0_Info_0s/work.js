c1ed5f386eaa095bd9da424de6181dd92a0337c5
7291990@daum.net <7291990@daum.net>
2024-08-08 18:45:45 +0900

    ddd

--- m0_Info_0s/work.js
+++ m0_Info_0s/work.js
@@ -1,8 +1,12 @@
 var work = [
 	/*기타*/
+  { "cate": "m01", "dep01": "냥냥붕어빵", "dep02": "인트로", "url": "chap001/sect001/game/index.html", "date": "2024-08-08", "status": ""
+  },
 	{ "cate": "m01", "dep01": "냥냥붕어빵", "dep02": "수와 연산 소인수분해", "url": "chap001/sect001/act01/index.html", "date": "2024-08-07", "status": ""
 	},
 
+  { "cate": "m02", "dep01": "당꾸를 찾아라", "dep02": "인트로", "url": "chap001/sect002/game/index.html", "date": "2024-08-08", "status": ""
+  },
 	{ "cate": "m02", "dep01": "당꾸를 찾아라", "dep02": "1. 소인수분해", "url": "chap001/sect002/act01/index.html", "date": "2024-08-07", "status": ""
 	},
 	{ "cate": "m02", "dep01": "당꾸를 찾아라", "dep02": "2. 최대공약수와 최소공배수", "url": "chap001/sect002/act02/index.html", "date": "2024-08-08", "status": ""
@@ -10,15 +14,23 @@ var work = [
 	{ "cate": "m02", "dep01": "당꾸를 찾아라", "dep02": "3. 정수와 유리수의 뜻과 대소 관계", "url": "chap001/sect002/act03/index.html", "date": "2024-08-08", "status": ""
 	},
 
-	{ "cate": "m03", "dep01": "테트라스퀘어로 몬드리안 따라하기", "dep02": "00", "url": "chap001/sect003/act01/index.html", "date": "2024-08-07", "status": ""
+  { "cate": "m03", "dep01": "테트라스퀘어로 몬드리안 따라하기", "dep02": "인트로", "url": "chap001/sect003/game/index.html", "date": "2024-08-08", "status": ""
+  },
+	{ "cate": "m03", "dep01": "테트라스퀘어로 몬드리안 따라하기", "dep02": "01", "url": "chap001/sect003/act01/index.html", "date": "2024-08-07", "status": ""
 	},
 
-	{ "cate": "m04", "dep01": "에라토스테네스의 체", "dep02": "00", "url": "chap001/sect004/act01/index.html", "date": "2024-08-07", "status": ""
+  { "cate": "m04", "dep01": "에라토스테네스의 체", "dep02": "인트로", "url": "chap001/sect004/game/index.html", "date": "2024-08-08", "status": ""
+  },
+	{ "cate": "m04", "dep01": "에라토스테네스의 체", "dep02": "01", "url": "chap001/sect004/act01/index.html", "date": "2024-08-07", "status": ""
 	},
 
+  { "cate": "m05", "dep01": "소수 찾기", "dep02": "인트로", "url": "chap001/sect005/game/index.html", "date": "2024-08-08", "status": ""
+  },
 	{ "cate": "m05", "dep01": "소수 찾기", "dep02": "01", "url": "chap001/sect005/act01/index.html", "date": "2024-08-08", "status": ""
 	},
 
+  { "cate": "m06", "dep01": "마법의 색칠 대모험", "dep02": "인트로", "url": "chap001/sect006/game/index.html", "date": "2024-08-08", "status": ""
+  },
 	{ "cate": "m06", "dep01": "마법의 색칠 대모험", "dep02": "01", "url": "chap001/sect006/act01/index.html", "date": "2024-08-08", "status": ""
 	},
 
