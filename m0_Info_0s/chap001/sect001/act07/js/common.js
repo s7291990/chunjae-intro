dac5f88eb124e06938a2ca2f2e6f765644e28e42
s7291990 <7291990@daum.net>
2024-08-09 23:55:19 +0900

    ddd

---
+++ m0_Info_0s/chap001/sect001/act07/js/common.js
@@ -0,0 +1,47 @@
+var currentPage = 0;
+var zoom = 1;
+var isFirst = true;
+var isPrevData = true;
+
+var pageValidation = []
+
+window.onload = () => {
+
+	contentResize();
+
+};
+
+
+function audioPlay(id) {
+	var audio = document.getElementById(id);
+	audio.play();
+}
+
+function popOpen(id){
+	$(id).addClass("on");
+	$(".contents").attr("aria-hidden","true");
+}
+function popClose(id){
+	$(id).removeClass("on");
+	$(".contents").attr("aria-hidden","false");
+}
+
+function handleRadio(id){
+	//audioPlay('audio01');
+	var selectValue = $(id + " input:checked").val();
+	if(selectValue !== ''){
+		$(id+" .btn-confirm").prop("disabled", false);
+	}
+}
+
+function handleSelect(id, n){
+  //audioPlay('audio01');
+  $(id + " .select-value").val(n);
+  $(id + " .select-cont-wrap button").removeClass("on");
+  $(id + " .select-cont-wrap button").eq(n-1).addClass("on");
+  $(id+" .btn-confirm").prop("disabled", false);
+}
+
+function handleOut(){
+	history.back();
+}
